143855520f0e1fdaf7a16798d4414b3103a63f61
Valeria Chacón <94030653+valechq19@users.noreply.github.com>
2022-07-25 19:53:50 -0600

    Semana.5Final

--- InitializrSpringbootProject/src/main/java/com/tienda/controller/IndexController.java
+++ InitializrSpringbootProject/src/main/java/com/tienda/controller/IndexController.java
@@ -1,24 +1,26 @@
 
 package com.tienda.controller;
 
+import com.tienda.dao.ClienteDao;
 import com.tienda.domain.Cliente;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
-public class IndexController {
-    //Repeticiones HTTP (Get: Recuperar datos)
-    @GetMapping("/") 
-     public String inicio(Model model){
-            log.info("Ahora se usa arquitectura MVC");
-            Cliente cliente1 = new Cliente("Pedro","Gomez Contreras","pgomez@gmail.com","85789847");
-            Cliente cliente2=new Cliente("Rebeca","Arguedas Alfaro","raguedas@gmail.com","87889647");
-            var clientes = Arrays.asList(cliente1,cliente2);
-            model.addAttribute("clientes",clientes);
-            return"index";
+public class IndexController{
+
+    @Autowired
+    private ClienteDao clienteDao;
+    @GetMapping("/")
+    public String inicio(Model model){
+        log.info("Ahora se usa arquitectura MVC");
+       var clientes =clienteDao.findAll();
+       model.addAttribute("clientes",null);
+        return"index";
 }
 }
--- InitializrSpringbootProject/src/main/java/com/tienda/domain/Cliente.java
+++ InitializrSpringbootProject/src/main/java/com/tienda/domain/Cliente.java
@@ -1,19 +1,31 @@
 package com.tienda.domain;
 
+import java.io.Serializable;
+import javax.persistence.*;
 import lombok.Data;
 
 @Data
-public class Cliente {
+@Entity
+@Table(name="cliente")
+public class Cliente implements Serializable{
+    private static final long serialVersionUID = 1L;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idcliente;
     String nombre;
     String apellidos;
     String correo;
     String telefono;
-    public Cliente(){// Cómo se creó el otro constructor ... el default debe crearse.
-    }
-    public Cliente(String nombre,String apellidos,String correo,String telefono){
+    public Cliente(){// Cómo se creó el otro
+        // constructor ... el default debe crearse ...
+   }
+    public Cliente(String nombre,String apellidos,
+            String correo,String telefono){
         this.nombre=nombre;
+
         this.apellidos=apellidos;
         this.correo=correo;
         this.telefono=telefono;
+        
 }
     }
