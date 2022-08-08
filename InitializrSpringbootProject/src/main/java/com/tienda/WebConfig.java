6a3b0f89f5795c05f04b6c3ddf09bf1f26eaf123
Valeria Chacón <94030653+valechq19@users.noreply.github.com>
2022-08-07 21:49:54 -0600

    Semana9.ParteFinal

---
+++ InitializrSpringbootProject/src/main/java/com/tienda/WebConfig.java
@@ -0,0 +1,36 @@
+
+package com.tienda;
+
+import java.util.Locale;
+import org.springframework.boot.autoconfigure.web.WebProperties.LocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    
+    @Bean
+    
+    public LocaleResolver localResolver(){
+       var slr = new SessionLocaleResolver();
+       slr.setDefaultLocale(new Locale("es"));
+       return slr;
+    }
+    
+    @Bean 
+    public LocaleChangeInterceptor localeChanceInterceptor(){
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    
+    @Override
+    
+    public void addInterceptors (InterceptorRegistry registro){
+        registro.addInterceptor(localeChanceInterceptor());
+    }
+}
